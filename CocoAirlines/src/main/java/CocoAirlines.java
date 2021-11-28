eef17f36a9b12a813d84e9c16d74a32a3f3a1fd1
NADJIB LAZAAR <nadjib.lazaar@umontpellier.fr>
2021-11-28 18:49:07 +0100

    Update CocoAirlines.java

--- CocoAirlines/src/main/java/CocoAirlines.java
+++ CocoAirlines/src/main/java/CocoAirlines.java
@@ -28,17 +28,7 @@ public class CocoAirlines {
 		StringBuilder st = new StringBuilder(
 				String.format(model.getName() + "-- %s\n", inst.nb_dividers, " X ", inst.capacity));
 
-		while (model.getSolver().solve()) {
-
-			st.append("\t");
-			for (int i = 0; i < inst.nb_dividers; i++) {
-				st.append(dividers[i]).append("\t\t");
-			}
-		//	System.out.println(st.toString());
-			st.setLength(0);
-			if (!allSolutions)
-				break;
-		}
+				//solver call!
 		
 		model.getSolver().printStatistics();
 
@@ -50,41 +40,10 @@ public class CocoAirlines {
 		model = new Model("Aircraft Class Divider ");
 
 		// VARIABLES
-		// set of marks that should be put on the ruler
-		dividers = model.intVarArray("a", inst.nb_dividers, 0, inst.capacity, false);
-		// set of distances between two distinct marks
-		IntVar[] diffs = model.intVarArray("d", (inst.nb_dividers * (inst.nb_dividers - 1)) / 2, 0, inst.capacity,
-				false);
-
+		// here!
+		
 		// CONSTRAINTS
-		// the first mark is set to 0
-		model.arithm(dividers[0], "=", 0).post();
-		model.arithm(dividers[inst.nb_dividers - 1], "=", inst.capacity).post();
-
-		// exits constraints:
-		for (int i : inst.exits)
-			for (int j = 0; j < inst.nb_dividers; j++)
-				model.arithm(dividers[j], "!=", i).post();
-
-		for (int i = 0, k = 0; i < inst.nb_dividers - 1; i++) {
-			// // the mark variables are ordered
-			model.arithm(dividers[i + 1], ">", dividers[i]).post();
-			for (int j = i + 1; j < inst.nb_dividers; j++, k++) {
-				// declare the distance constraint between two distinct marks
-				model.scalar(new IntVar[] { dividers[j], dividers[i] }, new int[] { 1, -1 }, "=", diffs[k]).post();
-				// redundant constraints on bounds of diffs[k]
-				model.arithm(diffs[k], ">=", (j - i) * (j - i + 1) / 2).post();
-				model.arithm(diffs[k], "<=", dividers[inst.nb_dividers - 1], "-",
-						((inst.nb_dividers - 1 - j + i) * (inst.nb_dividers - j + i)) / 2).post();
-			}
-		}
-		// all distances must be distinct
-		model.allDifferent(diffs, "BC").post();
-		// symmetry-breaking constraints
-		model.arithm(diffs[0], "<", diffs[diffs.length - 1]).post();
-
-		// business class constraint
-		model.arithm(diffs[0], ">", 1).post();
+		// here!
 
 	}
 
